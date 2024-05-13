e70f66f2242193f9694138d6a16a1ee7e9bb717f
Angel Aguero <angelagueror@gmail.com>
2024-05-13 19:15:13 -0400

    Resolved duplicate query results and improved URL shortening functionality
    - Updated repository query to ensure unique results when searching by key, resolving "Query did not return a unique result" errors.
    - Implemented redirection controller to handle redirects to full URLs based on shortened keys.
    - Enhanced response handling for redirections (HTTP 302) and JSON responses with full URL details.

--- src/main/java/com/github/angel/raa/service/UrlShortenerService.java
+++ src/main/java/com/github/angel/raa/service/UrlShortenerService.java
@@ -3,7 +3,6 @@ package com.github.angel.raa.service;
 import com.github.angel.raa.dto.Response;
 import com.github.angel.raa.dto.ShortUrlRequest;
 import com.github.angel.raa.dto.ShortUrlResponse;
-import org.jetbrains.annotations.NotNull;
 
 public interface UrlShortenerService {
     Response<ShortUrlResponse> createShortUrl(ShortUrlRequest url);
