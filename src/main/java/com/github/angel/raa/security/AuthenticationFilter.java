5ca6e1348ee12bcffd9087b547ec0d03b1770ef3
Angel Aguero <angelagueror@gmail.com>
2024-05-22 21:04:49 -0400

    Fix cycle of dependencies and bean lookup error in CustomCorsConfigurationSource.
    Resolved the cycle of dependencies between AuthenticationFilter, AuthenticationManager, and HttpSecurityConfiguration in the Spring application context. Also fixed the error where the bean 'corsConfigurationSource' could not be found by ensuring its configuration is loaded before creating 'corsFilter' in CustomCorsConfigurationSource.

--- src/main/java/com/github/angel/raa/security/AuthenticationFilter.java
+++ src/main/java/com/github/angel/raa/security/AuthenticationFilter.java
@@ -11,9 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +22,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 import static java.time.LocalDateTime.now;
+
 @RequiredArgsConstructor
 @Component
 public class AuthenticationFilter extends OncePerRequestFilter {
@@ -33,7 +32,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain chain) throws IOException, ServletException {
        String header = request.getHeader("Authorization");
        if(header == null || !header.startsWith("Bearer ")){
            chain.doFilter(request, response);
